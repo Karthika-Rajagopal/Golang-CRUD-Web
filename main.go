18d13adbe2bbb2230ad46a292e7863e2c9cbf66a
Karthika-Rajagopal <karthiii.884@gmail.com>
2023-04-14 21:36:58 +0530

    update form

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"e/config"
+	"e/controllers/categorycontroller"
+	"e/controllers/homecontroller"
+	"e/controllers/productcontroller"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// 	Database connection
+	config.ConnectDB()
+
+	// Routes
+	// 1.Homepage
+	http.HandleFunc("/", homecontroller.Welcome)
+
+	// 2. Category
+	http.HandleFunc("/categories", categorycontroller.Index)
+	http.HandleFunc("/categories/add", categorycontroller.Add)
+	http.HandleFunc("/categories/edit", categorycontroller.Edit)
+	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	// 3. Products
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
+
+	// Run server
+	log.Println("Server running on port: 9090")
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
